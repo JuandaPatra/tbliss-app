b80e0c288c0b7df181245545a722700f9ca03857
Rahmad Juanda Patra <patrajuanda10@gmail.com>
2023-08-02 13:44:12 +0700

    major update slider detail trip mobile

--- resources/views/web/detailtrip/index.blade.php
+++ resources/views/web/detailtrip/index.blade.php
@@ -30,11 +30,11 @@
                 </div>
                 <div>
                     @for($i=1; $i<=$detailTrip->trip_star; $i++)
-                    <svg aria-hidden="true" class="w-[15px] lg:w-5 h-5 text-[#5ec4dc] inline" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <title>First star</title>
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg>
-                    @endfor                   
+                        <svg aria-hidden="true" class="w-[15px] lg:w-5 h-5 text-[#5ec4dc] inline" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <title>First star</title>
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                        </svg>
+                        @endfor
                 </div>
             </div>
 
@@ -80,7 +80,7 @@
                     @endif
                     @endforeach
                 </h1>
-                
+
                 @foreach($detailTrip->picked_hidden_gems as $hidden_gem)
                 <p class="text-[16px] mt-[5px]">{{$hidden_gem}}</p>
                 @endforeach
@@ -364,17 +364,25 @@
     $(document).ready(function() {
 
         let allSlider = ('.city-slider').length;
-        console.log(allSlider)
 
         $('.slider').each(function(item, index) {
             // let dataIndex = (this).data("slider")
             let dataIndex = $(this).data("slider")
-            console.log(dataIndex)
 
             $(`.city-slider-${dataIndex}`).slick({
                 dots: false,
                 infinite: false,
                 slidesToShow: 2.5,
+                responsive: [
+                    {
+                        breakpoint: 800,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                        },
+                    },
+                    
+                ],
             });
             $(`.city-slider-${dataIndex}`).not('.slick-initialized').slick();
 
@@ -389,11 +397,11 @@
             }],
         };
         // $('.hg-slider').not('.slick-initialized').slick();
-        
+
         var slicky = '';
-        if ($(window).width() < 750){
+        if ($(window).width() < 750) {
             var slicky = $('.other-trip-section');
-        }else{
+        } else {
             var slicky = 'tes'
         }
         slicky.slick({
@@ -419,13 +427,13 @@
 
                 if ($(window).width() < 750 && !slicky.hasClass("slick-initialized")) {
                     slicky.slick(options);
-                }else{
+                } else {
                     slicky.slick("unslick");
                 }
             }, 100);
         });
 
-        
+
 
 
 
